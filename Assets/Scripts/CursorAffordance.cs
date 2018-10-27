6e716dc5f854e13ac54ee2b962805ec2c77db397
JD <jakub.dockal@gmail.com>
2018-10-27 12:10:56 +0200

    cursor change

---
+++ Assets/Scripts/CursorAffordance.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorAffordance : MonoBehaviour {
+
+	[SerializeField] Texture2D enemyCursor = null;
+	[SerializeField] Texture2D walkableCursor = null;
+	[SerializeField] Texture2D unknownCursor = null;
+
+	CameraRaycaster cameraRaycaster;
+
+	// Use this for initialization
+	void Start ()
+	{
+		cameraRaycaster = GetComponent<CameraRaycaster>();		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print(cameraRaycaster.layerHit);
+		SetCursor();
+	}
+
+	private void SetCursor()
+	{
+		switch (cameraRaycaster.layerHit)
+		{
+			case Layer.Walkable:
+				Cursor.SetCursor(walkableCursor, Vector2.zero, CursorMode.Auto);
+				break;
+
+			case Layer.Enemy:
+				Cursor.SetCursor(enemyCursor, Vector2.zero, CursorMode.Auto);
+				break;
+
+			default:
+				Cursor.SetCursor(unknownCursor, Vector2.zero, CursorMode.Auto);
+				return;
+		}
+	}
+}
