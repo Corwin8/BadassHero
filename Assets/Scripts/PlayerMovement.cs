ca026696eb357692b96191a2aa94d7665a737b35
JDHome <jakub.dockal@gmail.com>
2018-10-28 17:19:12 +0100

    movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,20 @@
 using System;
 using UnityEngine;
 using UnityStandardAssets.Characters.ThirdPerson;
+using UnityStandardAssets.CrossPlatformInput;
 
 [RequireComponent(typeof(ThirdPersonCharacter))]
 public class PlayerMovement : MonoBehaviour
 {
 	[SerializeField] float moveTolerance = 0.2f;
+	Vector3 currentMoveVector;
 
 
 	ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
     CameraRaycaster cameraRaycaster;
     Vector3 currentClickTarget;
+
+	bool isDirectMode = false;
         
     private void Start()
     {
@@ -22,6 +26,35 @@ public class PlayerMovement : MonoBehaviour
 	// Fixed update is called in sync with physics
 	private void FixedUpdate()
 	{
+		if (Input.GetKeyDown(KeyCode.G))
+		{
+			isDirectMode = !isDirectMode;
+		}
+
+		if (isDirectMode)
+		{
+			ProcessDirectMovement();
+		}
+		else
+		{
+			ProcessMouseMovement();
+		}
+
+	}
+
+	private void ProcessDirectMovement()
+	{
+		float h = CrossPlatformInputManager.GetAxis("Horizontal");
+		float v = CrossPlatformInputManager.GetAxis("Vertical");
+
+		Vector3 m_CamForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+		Vector3 m_Move = v * m_CamForward + h * Camera.main.transform.right;
+
+		m_Character.Move(m_Move, false, false);
+	}
+
+	private void ProcessMouseMovement()
+	{
 		if (Input.GetMouseButton(0))
 		{
 			switch (cameraRaycaster.layerHit)
@@ -29,15 +62,7 @@ public class PlayerMovement : MonoBehaviour
 				case Layer.Walkable:
 					{
 						currentClickTarget = cameraRaycaster.hit.point;
-						var currentMoveVector = currentClickTarget - transform.position;
-						if (currentMoveVector.magnitude >= moveTolerance)
-						{
-							m_Character.Move(currentMoveVector, false, false);
-						}
-						else
-						{
-							m_Character.Move(Vector3.zero, false, false);
-						}
+
 						break;
 					}
 				case Layer.Enemy:
@@ -51,8 +76,17 @@ public class PlayerMovement : MonoBehaviour
 						return;
 					}
 			}
-		}
 
+		}
+		currentMoveVector = currentClickTarget - transform.position;
+		if (currentMoveVector.magnitude >= moveTolerance)
+		{
+			m_Character.Move(currentMoveVector, false, false);
+		}
+		else
+		{
+			m_Character.Move(Vector3.zero, false, false);
+		}
 	}
 }
 
