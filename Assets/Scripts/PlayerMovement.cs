6f317793db8463bc5f9af10b78728ecaf2c536cb
JD <jakub.dockal@gmail.com>
2018-10-31 09:41:22 +0100

    gizmos

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,33 +6,32 @@ using UnityStandardAssets.CrossPlatformInput;
 [RequireComponent(typeof(ThirdPersonCharacter))]
 public class PlayerMovement : MonoBehaviour
 {
-	[SerializeField] float moveTolerance = 0.2f;
-	Vector3 currentMoveVector;
+	[SerializeField] float walkMoveStopRadius = 0.2f;
+	[SerializeField] float attackMoveStopRadius = 5f;
 
+	ThirdPersonCharacter thirdPersonCharacter;   // A reference to the ThirdPersonCharacter on the object
+	CameraRaycaster cameraRaycaster;
+	Vector3 currentDestination, clickPoint;
 
-	ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
-    CameraRaycaster cameraRaycaster;
-    Vector3 currentClickTarget;
+	bool isInDirectMode = false;
 
-	bool isDirectMode = false;
-        
-    private void Start()
-    {
-        cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
-        m_Character = GetComponent<ThirdPersonCharacter>();
-        currentClickTarget = transform.position;
-    }
+	private void Start()
+	{
+		cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
+		thirdPersonCharacter = GetComponent<ThirdPersonCharacter>();
+		currentDestination = transform.position;
+	}
 
 	// Fixed update is called in sync with physics
 	private void FixedUpdate()
 	{
-		if (Input.GetKeyDown(KeyCode.G))
+		if (Input.GetKeyDown(KeyCode.G)) // G for gamepad. TODO add to menu
 		{
-			isDirectMode = !isDirectMode;
-			currentClickTarget = transform.position;
+			isInDirectMode = !isInDirectMode; // toggle mode
+			currentDestination = transform.position; // clear the click target
 		}
 
-		if (isDirectMode)
+		if (isInDirectMode)
 		{
 			ProcessDirectMovement();
 		}
@@ -40,54 +39,72 @@ public class PlayerMovement : MonoBehaviour
 		{
 			ProcessMouseMovement();
 		}
-
 	}
 
 	private void ProcessDirectMovement()
 	{
-		float h = CrossPlatformInputManager.GetAxis("Horizontal");
-		float v = CrossPlatformInputManager.GetAxis("Vertical");
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
 
-		Vector3 m_CamForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
-		Vector3 m_Move = v * m_CamForward + h * Camera.main.transform.right;
+		// calculate camera relative direction to move:
+		Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+		Vector3 movement = v * cameraForward + h * Camera.main.transform.right;
 
-		m_Character.Move(m_Move, false, false);
+		thirdPersonCharacter.Move(movement, false, false);
 	}
 
 	private void ProcessMouseMovement()
 	{
 		if (Input.GetMouseButton(0))
 		{
+			clickPoint = cameraRaycaster.hit.point;
 			switch (cameraRaycaster.currentLayerHit)
 			{
 				case Layer.Walkable:
-					{
-						currentClickTarget = cameraRaycaster.hit.point;
-
-						break;
-					}
+					currentDestination = ShortDestination(clickPoint, walkMoveStopRadius);
+					break;
 				case Layer.Enemy:
-					{
-						print("Not moving towards Enemy.");
-						break;
-					}
+					currentDestination = ShortDestination(clickPoint, attackMoveStopRadius);
+					break;
 				default:
-					{
-						print("SHOULDN'T HAPPEN!");
-						return;
-					}
+					print("Unexpected layer found");
+					return;
 			}
-
 		}
-		currentMoveVector = currentClickTarget - transform.position;
-		if (currentMoveVector.magnitude >= moveTolerance)
+		WalkToDestination();
+	}
+
+	private void WalkToDestination()
+	{
+		var playerToClickPoint = currentDestination - transform.position;
+		if (playerToClickPoint.magnitude >= walkMoveStopRadius)
 		{
-			m_Character.Move(currentMoveVector, false, false);
+			thirdPersonCharacter.Move(playerToClickPoint, false, false);
 		}
 		else
 		{
-			m_Character.Move(Vector3.zero, false, false);
+			thirdPersonCharacter.Move(Vector3.zero, false, false);
 		}
 	}
+
+	Vector3 ShortDestination(Vector3 destination, float shortening)
+	{
+		Vector3 reductionVector = (destination - transform.position).normalized * shortening;
+		return destination - reductionVector;
+	}
+
+	void OnDrawGizmos()
+	{
+		// Draw movement gizmos
+		Gizmos.color = Color.green;
+		Gizmos.DrawLine(transform.position, clickPoint);
+		Gizmos.DrawSphere(currentDestination, 0.15f);
+		Gizmos.DrawSphere(clickPoint, 0.1f);
+
+		// Draw attack sphere
+		Gizmos.color = new Color(255f, 255f, 255f, .5f);
+		Gizmos.DrawWireSphere(transform.position, attackMoveStopRadius);
+	}
 }
+
 
